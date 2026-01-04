e742c8e8c7cc20e9452bcc12da4225931992dd4b
はなこくん <167955655+zolotayautka@users.noreply.github.com>
2026-01-04 17:39:04 +0900

    Add files via upload

--- FolygonDic Apple/FolygonDic/ContentView.swift
+++ FolygonDic Apple/FolygonDic/ContentView.swift
@@ -764,8 +764,8 @@ struct initModalView: View {
     @State private var gengo: String? = nil
     @State private var study_value: Int8 = 20
     @Binding var showModal: Bool
-    let items = ["Japanese".localized, "Korean".localized, "English".localized, "Russian".localized, "Chinese(Mainland)".localized, "Spanish(Spain)".localized]
-    let lang: [String : String] = ["Japanese".localized : "ja-JP", "Korean".localized : "ko-KR", "English".localized : "en-US", "Russian".localized : "ru-RU", "Chinese(Mainland)".localized : "zh-CN", "Spanish(Spain)".localized : "es-ES"]
+    let items = ["Japanese".localized, "Korean".localized, "English".localized, "Russian".localized, "Chinese".localized, "Spanish".localized]
+    let lang: [String : String] = ["Japanese".localized : "ja-JP", "Korean".localized : "ko-KR", "English".localized : "en-US", "Russian".localized : "ru-RU", "Chinese".localized : "zh-CN", "Spanish".localized : "es-ES"]
     var body: some View {
         VStack {
             HStack {
